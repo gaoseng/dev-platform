e1f53efdf55646d28479a42b0e992374191ff966
gaosen <gaoqs@fastlion.cn>
2019-05-30 16:46:56 +0800

    配置karma 环境

---
+++ test/test.spec.js
@@ -0,0 +1,8 @@
+describe('unit test', () => {
+    it('shuold 1 equal 1', () => {
+        expect(1).toBe(1);
+    });
+    // it(' 1 equal not 2 ', () => {
+    //     expect(1).toBe(2);
+    // })
+   })
